580f5b9edc3d44fdec251e29d044025876847b49
ABHISHEK KUMAR <110424839+Abhi291712@users.noreply.github.com>
2024-07-21 20:41:03 -0700

    Create word.py

---
+++ model/word.py
@@ -0,0 +1,6 @@
+from .lexical_entry import LexicalEntry
+
+class Word:
+    def __init__(self, lex_entry: LexicalEntry, surface_form: str):
+        self.lex_entry = lex_entry
+        self.surface_form = surface_form
