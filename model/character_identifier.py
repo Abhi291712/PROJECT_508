dc44ba1f77427b2bb7367d3d897e25f06f255fb0
ABHISHEK KUMAR <110424839+Abhi291712@users.noreply.github.com>
2024-07-21 21:25:46 -0700

    Update character_identifier.py

--- model/character_identifier.py
+++ model/character_identifier.py
@@ -1,13 +1,1 @@
-from typing import Dict, Optional
-from model.common_enums import CharacterType
-from model.lexical_entry import LexicalEntry, Translation
 
-class CharacterIdentifier:
-    def __init__(self):
-        self.char_info: Dict[str, LexicalEntry] = {
-            "अ": LexicalEntry("अ", "a", CharacterType.VOWEL, "/ə/", Translation("a", "The first letter of the Devanagari script")),
-            # Add more characters here
-        }
-
-    def identify(self, char: str) -> Optional[LexicalEntry]:
-        return self.char_info.get(char)
